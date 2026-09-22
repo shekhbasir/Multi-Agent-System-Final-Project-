c332314d60021c1fe930b68ddd1833ee0d1b1631
shekh basir <shekhbasir9821273349@gmail.com>
2026-09-22 18:24:52 +0530

    Update ZEGO_APP_ID and ZEGO_SERVER_SECRET values

--- Frontend/basir/src/config/zego.js
+++ Frontend/basir/src/config/zego.js
@@ -2,8 +2,8 @@
 
 // export const ZEGO_SERVER_SECRET = import.meta.env.VITE_ZEGO_SERVER_SECRET;
 
-export const ZEGO_APP_ID = 1112638014;
+export const ZEGO_APP_ID = 1942790284;
 
-export const ZEGO_SERVER_SECRET = "dbb389236e73da8549a261987dd1df58";
+export const ZEGO_SERVER_SECRET = "3e9773cf0c9b5e563e6fe4253c584563";
 
 /// this will going to handle the data and the scarsity of teh code
